ec3fdd20993051bb51911ebf8a957125d20b6bef
Steve Kinney <hello@stevekinney.net>
2015-12-05 10:35:17 -0700

    Get basic functionality up and running

--- app/models/album.js
+++ app/models/album.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
+  year: DS.attr('string'),
   artist: DS.belongsTo('artist', { async: true }),
   songs: DS.hasMany('song', { async: true })
 });
--- app/router.js
+++ app/router.js
@@ -6,6 +6,14 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('artists', function() {
+    this.route('artist', { path: ':artist_id' }, function() {
+      this.route('albums', function() {
+        this.route('album', { path: ':album_id' }, function() {
+        })
+      })
+    });
+  });
 });
 
 export default Router;
